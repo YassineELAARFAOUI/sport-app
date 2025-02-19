27035e930f6abca6029ab6fe2785bd833344f643
Yassine ELAARFAOUI <elaarfaoui.yassine2@gmail.com>
2025-02-19 11:49:22 +0100

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  distDir: 'out', // Ensures the build output is in "out/"
-  trailingSlash: true, // Fixes routing issues
-};
+const nextConfig = {};
 
 export default nextConfig;
